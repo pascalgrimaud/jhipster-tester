3fe621b9d88ff49fda2b896102cf4c76eb7cff25
Pascal Grimaud <pascalgrimaud@users.noreply.github.com>
2016-05-30 15:38:46 +0200

    chmod m2

--- travis/install/04-cache.sh
+++ travis/install/04-cache.sh
@@ -8,6 +8,7 @@ git clone -b $JHIPSTER_CACHE_BRANCH $JHIPSTER_CACHE_REPO
 
 rm -Rf "$HOME"/.m2/repository/
 mv "$TRAVIS_BUILD_DIR"/jhipster-travis-cache/repository "$HOME"/.m2/
+chmod -R 777 "$HOME"/.m2/
 
 #-------------------------------------------------------------------------------
 # Use phantomjs cache
