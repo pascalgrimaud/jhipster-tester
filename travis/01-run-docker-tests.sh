2b20b44277fdb40a3e76b01c26b05a33f9921649
Pascal Grimaud <pascalgrimaud@users.noreply.github.com>
2016-05-30 17:18:16 +0200

    Test skip front

--- travis/01-run-docker-tests.sh
+++ travis/01-run-docker-tests.sh
@@ -37,15 +37,30 @@ runContainerSkipFront() {
         -e JHIPSTER_REPO_URL=https://github.com/pascalgrimaud/generator-jhipster.git \
         -e JHIPSTER_REPO_BRANCH=master \
         -e JHIPSTER_TEST_FRONT=0
+        -e JHIPSTER_TEST_PACKAGING=0
+        -t jhipster-tester
+}
+
+runContainerSkipFrontNoDaemon() {
+    local application="$1"
+    docker run \
+        --name $application --label=test \
+        -v "$JHIPSTER_TRAVIS"/samples/"$application":/home/jhipster/volume/app:ro \
+        -v "$HOME"/.m2:/home/jhipster/volume/.m2:ro \
+        -v "$TRAVIS_BUILD_DIR":/home/jhipster/volume/generator-jhipster:ro \
+        -e JHIPSTER_REPO_URL=https://github.com/pascalgrimaud/generator-jhipster.git \
+        -e JHIPSTER_REPO_BRANCH=master \
+        -e JHIPSTER_TEST_FRONT=0
+        -e JHIPSTER_TEST_PACKAGING=0
         -t jhipster-tester
 }
 
 #-------------------------------------------------------------------------------
 
 cd "$JHIPSTER_TRAVIS"
-runContainer app-psql-es-noi18n
-runContainer app-mongodb
-runContainer app-cassandra
-runContainerNoDaemon app-mysql
+runContainerSkipFront app-psql-es-noi18n
+runContainerSkipFront app-mongodb
+runContainerSkipFront app-cassandra
+runContainerSkipFrontNoDaemon app-mysql
 # runContainer app-gateway
 # runContainer app-hazelcast-cucumber
